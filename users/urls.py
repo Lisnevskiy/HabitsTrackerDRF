8cd5a0f29d93b7ec36d969510a9c8982f569aae3
Alexander <Lisnevskiy14@gmail.com>
2023-10-09 11:11:05 +0300

    Added User urls

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from users.apps import UsersConfig
+from users.views import UserRegisterView
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path('register/', UserRegisterView.as_view(), name='users_register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
