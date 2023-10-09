4c0d4ab21284b9b90a2d1836ca8429b55d38d6c0
Alexander <Lisnevskiy14@gmail.com>
2023-10-09 11:06:15 +0300

    Update Habit urls

--- habit/urls.py
+++ habit/urls.py
@@ -1,8 +1,14 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from habit.views import HabitViewSet
+from habit.apps import HabitConfig
+from habit.views import HabitViewSet, HabitPublicListView
+
+app_name = HabitConfig.name
 
 router = DefaultRouter()
-router.register(r'habit', HabitViewSet)
+router.register(r'habit', HabitViewSet, basename='habit')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('habit/public/', HabitPublicListView.as_view(), name='habit_public')
+] + router.urls
