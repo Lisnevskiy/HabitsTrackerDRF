5537d84fea5e645ccbb95b5021b0b48bd46aad76
Alexander <Lisnevskiy14@gmail.com>
2023-10-09 11:03:09 +0300

    Update Habit views

--- habit/views.py
+++ habit/views.py
@@ -1,9 +1,26 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 
 from habit.models import Habit
+from habit.paginators import HabitSetPagination
+from habit.permissions import IsOwner
 from habit.serializers import HabitSerializer
 
 
 class HabitViewSet(viewsets.ModelViewSet):
-    queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+    pagination_class = HabitSetPagination
+    permission_classes = (IsOwner, IsAuthenticated)
+
+    def get_queryset(self):
+        return Habit.objects.filter(user=self.request.user.pk)
+
+
+class HabitPublicListView(generics.ListAPIView):
+    queryset = queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    pagination_class = HabitSetPagination
+
+    def get_queryset(self):
+        return Habit.objects.filter(is_public=True)
+    
