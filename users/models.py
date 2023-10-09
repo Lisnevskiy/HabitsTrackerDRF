0c4f5a6acc48d4830fad556c5015ee491e572c19
Alexander <Lisnevskiy14@gmail.com>
2023-10-09 11:07:53 +0300

    Added User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    tg_chat_id = models.BigIntegerField(verbose_name='ID чата пользователя бота', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
